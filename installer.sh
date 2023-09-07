9caa0c99c2bdef76d751542b325afa07b1c4670e
MohamadReza Moghadasi <30479983+mrmoghadasi@users.noreply.github.com>
2023-09-07 15:47:24 +0330

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+mkdir -p /var/lib/node_exporter/textfile_collector/ufw.prom
+mkdir -p /usr/lib/ufw_status/
+
+cp ufw_status.sh /usr/lib/ufw_status/
+chmod + x /usr/lib/ufw_status/ufw_status.sh
+
+
+echo '[Unit]
+Description=Run ufw_status.sh every 5 seconds
+
+[Service]
+ExecStart=/usr/lib/ufw_status/ufw_status.sh
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/ufw_status.service
+
+echo '[Unit]
+Description=Run ufw_status.service every 5 seconds
+
+[Timer]
+OnBootSec=5s
+OnUnitActiveSec=5s
+
+[Install]
+WantedBy=timers.target; > /etc/systemd/system/ufw_status.timer
+
+
+
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable ufw_status.service
+sudo systemctl enable ufw_status.timer
+
+sudo systemctl start ufw_status.service
+sudo systemctl start ufw_status.timer
+
+sudo systemctl status ufw_status.service
+sudo systemctl status ufw_status.timer
+
+
+
